9d4cc16b7776d862294e934f46c2f771f7c20285
Karma S Ghale <karma.ghale1990@gmail.com>
2024-04-06 13:35:23 -0400

    clicking All Raffles on nav shows all the available raffles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import SingleRaffle from "./Components/SingleRaffle/SingleRaffle";
+import All_Raffles from "./Components/All_Raffles/All_Raffles";
 import RaffleParticipants from "./Components/RaffleParticipants/RaffleParticipants";
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/raffles" element={<All_Raffles />}></Route>
           <Route path="/raffles/:id" element={<SingleRaffle />}></Route>
           <Route
             path="/raffles/:id/participants"
